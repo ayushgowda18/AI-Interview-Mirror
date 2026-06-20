ab5b63a7773e02befb4af3da65a8359b6fb1e71c
ayushgowda18 <ayushgowdar18@gmail.com>
2026-06-20 21:57:20 +0530

    i missed this

--- AI-Interview-Mirror/frontend/src/pages/Dashboard.jsx
+++ AI-Interview-Mirror/frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,23 @@
 function Dashboard() {
+  const username = localStorage.getItem("username");
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+
+    window.location.href = "/login";
+  };
   return (
     <div>
-      <h1>Dashboard</h1>
+      
+      <h2>Welcome back, {username}! 👋</h2>
+
+      <p>
+        Ready to ace your next interview?
+      </p>
+      <button onClick={handleLogout}>
+        Logout
+      </button>
+      
     </div>
   );
 }
